709d48a80afbd45b18a01028010ac42cb71746c2
noodlefreeze <noodlefreeze@gmail.com>
2025-02-26 20:41:56 +0800

    Leature 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,36 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      HStack {
-        CardView(isFaceUp: true)
-        CardView()
-        CardView()
+  let emojis = [
+    "🥳", "🙈", "💁", "🤩", "⚽️", "🛹", "⛷️", "⛷️", "⛷️", "⛷️", "⛷️", "⛷️", "⛷️",
+  ]
+  @State var cardCount = 4
+
+  var body: some View {
+    VStack {
+      ScrollView {
+        cards
+      }
+      Spacer()
+      cardCountAdjusters
+    }
+    .padding()
+  }
+
+  var cardCountAdjusters: some View {
+    HStack {
+      cardAdder
+      Spacer()
+      cardRemover
+    }
+    .imageScale(.large)
+  }
+
+  func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+    Button(
+      action: {
+        cardCount += offset
+      },
+      label: {
+        Image(systemName: symbol)
+      }
+    )
+    .disabled(cardCount + offset > emojis.count || cardCount + offset < 1)
+  }
+
+  var cardAdder: some View {
+    cardCountAdjuster(by: 1, symbol: "folder.badge.plus")
+
+  }
+
+  var cardRemover: some View {
+    cardCountAdjuster(by: -1, symbol: "folder.badge.minus")
+  }
+
+  var cards: some View {
+    LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+      ForEach(0..<cardCount, id: \.self) { index in
+        CardView(content: emojis[index])
+          .aspectRatio(2 / 3, contentMode: .fit)
       }
     }
+    .foregroundStyle(.orange)
+  }
 }
 
 struct CardView: View {
-  var isFaceUp = false
-  
+  let content: String
+  @State var isFaceUp = false
+
   var body: some View {
-    ZStack{
-      if isFaceUp {
-        RoundedRectangle(cornerRadius: 12)
-          .foregroundColor(.white)
-        RoundedRectangle(cornerRadius: 12)
+    ZStack {
+      let base = RoundedRectangle(cornerRadius: 12)
+
+      Group {
+        base
+          .fill(.white)
+        base
           .strokeBorder(lineWidth: 2)
-        Text("🥹")
+        Text(content)
           .font(.largeTitle)
-      } else {
-        RoundedRectangle(cornerRadius: 12)
       }
+      .opacity(isFaceUp ? 1 : 0)
+      base.fill().opacity(isFaceUp ? 0 : 1)
     }
-    .foregroundColor(.orange)
-    .padding()
+    .onTapGesture {
+      isFaceUp.toggle()
+    }
   }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
